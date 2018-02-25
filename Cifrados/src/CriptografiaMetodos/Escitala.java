bd6a708ca824ece071767c2606ced49088b313bc
nand05 <36611600+nand05@users.noreply.github.com>
2018-02-25 14:25:13 -0600

    Add files via upload

--- Cifrados/src/CriptografiaMetodos/Escitala.java
+++ Cifrados/src/CriptografiaMetodos/Escitala.java
@@ -11,29 +11,29 @@ public class Escitala {
 	static int caras;
 	static int largo;
 	static int val = 0;
-	static char [][] matriz;
-	static char [][] matrizT;
-	
-	public double largoMensaje(String mensaje){
+	static char[][] matriz;
+	static char[][] matrizT;
+
+	public double largoMensaje(String mensaje) {
 		int cadena;
 		double num = 0;
 		FileReader f;
 		try {
 			f = new FileReader(mensaje);
 			cadena = f.read();
-	        while (cadena != -1) {
-	        	num += 1;
+			while (cadena != -1) {
+				num += 1;
 				cadena = f.read();
 			}
-	        f.close();
+			f.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return num;
 	}
-	
-	public static void llenaMatriz(int filas, int columnas, String mensaje){
-		matriz = new char [filas][columnas];
+
+	public static void llenaMatriz(int filas, int columnas, String mensaje) {
+		matriz = new char[filas][columnas];
 		int cadena;
 		FileReader f;
 		try {
@@ -42,84 +42,83 @@ public class Escitala {
 			for (int i = 0; i < filas; i++) {
 				for (int j = 0; j < columnas; j++) {
 					if (cadena == -1) {
-						matriz[i][j] = (char)32;
+						matriz[i][j] = (char) 32;
 					} else {
-						matriz[i][j] = (char)cadena;
+						matriz[i][j] = (char) cadena;
 						cadena = f.read();
 					}
 				}
 			}
-	        f.close();
+			f.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public static void transpuesta(char [][] mat) {
+
+	public static void transpuesta(char[][] mat) {
 		if (val != 0) {
-			matrizT = new char [largo][caras];
+			matrizT = new char[largo][caras];
 			for (int i = 0; i < mat[0].length; i++) {
 				for (int j = 0; j < caras; j++) {
 					matrizT[i][j] = mat[j][i];
 				}
 			}
-		}else {
-			matrizT = new char [caras][largo];
+		} else {
+			matrizT = new char[caras][largo];
 			for (int i = 0; i < mat.length; i++) {
 				for (int j = 0; j < caras; j++) {
 					matrizT[j][i] = mat[i][j];
 				}
 			}
 		}
-		
+
 	}
-	
-	public  void Cifrar(String dir, int numCaras) {
-		
+
+	public void Cifrar(String dir, int numCaras) {
+
 		FileWriter fichero = null;
-        PrintWriter pw = null;
+		PrintWriter pw = null;
 		msj = dir;
 		caras = numCaras;
-		largo = (int)Math.ceil(largoMensaje(msj) / numCaras);
-		llenaMatriz(largo,caras,msj);
+		largo = (int) Math.ceil(largoMensaje(msj) / numCaras);
+		llenaMatriz(largo, caras, msj);
 		transpuesta(matriz);
 		try {
-			fichero = new FileWriter(msj.substring(0, msj.length()-4)+"C.txt");
-            pw = new PrintWriter(fichero);
-            for (int i = 0; i < caras; i++) {
+			fichero = new FileWriter(msj.substring(0, msj.length() - 4) + "C.txt");
+			pw = new PrintWriter(fichero);
+			for (int i = 0; i < caras; i++) {
 				for (int j = 0; j < largo; j++) {
 					pw.write(matrizT[i][j]);
 				}
 			}
-            fichero.close();
-            matriz = null;
-            matrizT = null;
+			fichero.close();
+			matriz = null;
+			matrizT = null;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void Descifrar(String dir, int numCaras) {
 		val = 1;
 		FileWriter fichero = null;
-        PrintWriter pw = null;
+		PrintWriter pw = null;
 		msj = dir;
 		caras = numCaras;
-		largo = (int)Math.ceil(largoMensaje(msj) / numCaras);
-		llenaMatriz(caras,largo,msj);
+		largo = (int) Math.ceil(largoMensaje(msj) / numCaras);
+		llenaMatriz(caras, largo, msj);
 		transpuesta(matriz);
 		try {
-			fichero = new FileWriter(msj.substring(0, msj.length()-5)+"D.txt");
-            pw = new PrintWriter(fichero);
-            for (int i = 0; i < matrizT.length; i++) {
+			fichero = new FileWriter(msj.substring(0, msj.length() - 5) + "D.txt");
+			pw = new PrintWriter(fichero);
+			for (int i = 0; i < matrizT.length; i++) {
 				for (int j = 0; j < caras; j++) {
 					pw.write(matrizT[i][j]);
 				}
 			}
-            fichero.close();
-            matriz = null;
-            matrizT = null;
+			fichero.close();
+			matriz = null;
+			matrizT = null;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
